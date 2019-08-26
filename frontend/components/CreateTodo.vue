862d69f68196798854cd39f86e25264245bcfa76
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-08-26 18:42:14 +0200

    Create Todo component

--- frontend/components/CreateTodo.vue
+++ frontend/components/CreateTodo.vue
@@ -1,59 +1,57 @@
 <template>
   <div class="w-25 mx-auto">
     <h2>Create a TODO List</h2>
-    <b-form @submit="onSubmit" @reset="onReset">
+    <b-form>
+      <span
+        v-show="typing"
+        class="help-block small text-center"
+      >Hit enter to save</span>
       <b-form-input
         id="newTodo"
         v-model="form.todo"
         required
         placeholder="Enter something todo"
-        @keypress="typing=true"
+        @input="typing = true"
+        @keydown.enter="addTodo($event)"
       />
-      <span v-show="typing" class="help-block small text-center">Hit enter to save</span>
     </b-form>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 
 export default {
-  data() {
-    return {
-      form: {
-        todo: ""
-      },
-      typing: true
-    };
-  },
-  methods: {
-    // addTodo(event) {
-    //     if (event) {
-    //         event.preventDefault();
-    //     }
-    //     const url = 'http://localhost:4000/api/add';
-    //     const param = {
-    //         name: this.todo,
-    //         done: 0
-    //     };
-    //     axios
-    //         .post(url, param)
-    //         .then((response) => {
-    //             console.log(response);
-    //             this.clearTodo();
-    //             this.refreshTodo();
-    //             this.typing = false;
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    // },
-    // clearTodo() {
-    //     this.todo = '';
-    // },
-    // refreshTodo() {
-    //     // bus.$emit("refreshTodo");
-    // }
-  }
+    data() {
+        return {
+            form: {
+                todo: ''
+            },
+            todos: '',
+            typing: false
+        };
+    },
+    methods: {
+        async addTodo(event) {
+            if (event) {
+                event.preventDefault();
+            }
+            this.typing = false;
+            const param = {
+                task: this.form.todo 
+            };
+            console.log('param add', param);
+			
+            this.$store.dispatch('todo/ADD_TODO_LIST', param);
+            this.clearTodo();
+            // this.refreshTodo();
+        },
+        clearTodo() {
+            this.form.todo = '';
+        },
+        refreshTodo() {
+            // this.$store.dispatch('todo/FETCH_TODO_LIST');
+        },
+    }
 };
 </script>
